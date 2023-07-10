13ed8be913d9821f04c22a24b502704496e6c417
Theo <58122097+tzebre@users.noreply.github.com>
2023-07-10 15:49:25 -0400

    corected typo dice_y

--- utilities/dice_y_value.py
+++ utilities/dice_y_value.py
@@ -6,7 +6,7 @@ from matplotlib.colors import ListedColormap
 
 
 def get_parser():
-    parser = argparse.ArgumentParser(description='Get dice score on Y axis')
+    parser = argparse.ArgumentParser(description='Get f1 score on Y axis')
     parser.add_argument('-gt', required=True, help='Path to the ground truth')
     parser.add_argument('-pr', required=True, help='Path to the predicted label')
     parser.add_argument('-im', required=True, help='Path to the original image')
@@ -44,13 +44,13 @@ def fn_fp_slice(image_slice, mri):
 
 def tp_slice(ground_truth, label, mri):
     """
-    From one slice create image with TP, FP, TN, FN voxels and calcul dice score.
+    From one slice create image with TP, FP, TN, FN voxels and calcul f1 score.
     Args:
         ground_truth (np.array): Slice of ground truth
         label (np.array): Slice of prediction
         mri (np.array): Slice of original
     Returns:
-        dice (float): Dice score for the slice
+        f1 (float): Dice score for the slice
         cropped_ground_truth_data (np.array): Cropped ground truth around ROI
         colors (np.array): Image with voxel value specific to TP, FP, TN, FN
         cropped_mri_data (np.array): Cropped mri around ROI
@@ -102,7 +102,7 @@ if __name__ == '__main__':
     min_val = min(min(z_slice_val_label), min(z_slice_val_img))
     max_val = max(max(z_slice_val_label), max(z_slice_val_img))
     res_dict = {"TP": [[], 0], "FP": [[], 0], "TN": [[], 0], "FN": [[], 0]}
-    all_dice = {"TP": {}, "FN": {}, "FP": {}}
+    all_f1 = {"TP": {}, "FN": {}, "FP": {}}
     for z_slice in range(min_val, max_val):
         ground_truth = np.any(z_slice_val_img == z_slice)
         PRED = np.any(z_slice_val_label == z_slice)
@@ -110,57 +110,57 @@ if __name__ == '__main__':
             if PRED:
                 res_dict["TP"][0].append(z_slice)
                 res_dict["TP"][1] += 1
-                dice, ground_truth, pred, base = tp_slice(image_dt[:, :, z_slice], label_dt[:, :, z_slice],
+                f1, ground_truth, pred, base = tp_slice(image_dt[:, :, z_slice], label_dt[:, :, z_slice],
                                                           mri_dt[:, :, z_slice])
-                all_dice["TP"][z_slice] = (dice, ground_truth, pred, base)
+                all_f1["TP"][z_slice] = (f1, ground_truth, pred, base)
             else:
                 res_dict["FN"][0].append(z_slice)
                 res_dict["FN"][1] += 1
                 img, base = fn_fp_slice(image_dt[:, :, z_slice], mri_dt[:, :, z_slice])
-                all_dice["FN"][z_slice] = (0, img, 0, base)
+                all_f1["FN"][z_slice] = (0, img, 0, base)
         else:
             if PRED:
                 res_dict["FP"][0].append(z_slice)
                 res_dict["FP"][1] += 1
                 img, base = fn_fp_slice(label_dt[:, :, z_slice], mri_dt[:, :, z_slice])
-                all_dice["FP"][z_slice] = (0, img, 0, base)
+                all_f1["FP"][z_slice] = (0, img, 0, base)
             else:
                 res_dict["TN"][0].append(z_slice)
                 res_dict["TN"][1] += 1
     for k in res_dict:
         print(f"{k}:{res_dict[k][1]}")
-    Dice_z_slice = f"Dice on z_slice axis : {(2 * res_dict['TP'][1]) / (2 * res_dict['TP'][1] + res_dict['FP'][1] + res_dict['FN'][1])}"
+    Dice_z_slice = f"Z-axis F1 score : {(2 * res_dict['TP'][1]) / (2 * res_dict['TP'][1] + res_dict['FP'][1] + res_dict['FN'][1])}"
     sensi = f"Sensitivity: {res_dict['TP'][1] / (res_dict['TP'][1] + res_dict['FN'][1])}"  # good detection of real case
     speci = f"Specificity: {res_dict['TN'][1] / (res_dict['TN'][1] + res_dict['FP'][1])}"  # good detection of negative case
 
     for type in ["TP", "FP", "FN"]:
-        fig_width = len(all_dice[type]) * 2
+        fig_width = len(all_f1[type]) * 2
         fig_height = 4
         if type == "TP":
-            fig, axes = plt.subplots(len(all_dice[type]), 3, figsiz_slicee=(fig_height, fig_width))
-            z_slice_dice = []
+            fig, axes = plt.subplots(len(all_f1[type]), 3, figsize=(fig_height, fig_width))
+            z_slice_f1 = []
         else:
-            fig, axes = plt.subplots(len(all_dice[type]), 2, figsiz_slicee=(fig_height, fig_width))
+            fig, axes = plt.subplots(len(all_f1[type]), 2, figsize=(fig_height, fig_width))
 
-        for i, slice in enumerate(all_dice[type]):
-            axes[i, 0].imshow(all_dice[type][slice][1], cmap='gray')
+        for i, slice in enumerate(all_f1[type]):
+            axes[i, 0].imshow(all_f1[type][slice][1], cmap='gray')
             axes[i, 0].axis('off')
-            axes[i, 1].imshow(all_dice[type][slice][3], cmap='gray')
+            axes[i, 1].imshow(all_f1[type][slice][3], cmap='gray')
             axes[i, 1].axis('off')
             if type == "TP":
                 colors_cmap = ['black', 'orange', 'red', 'green']
                 custom_cmap = ListedColormap(colors_cmap)
-                axes[i, 2].imshow(all_dice["TP"][slice][2], cmap=custom_cmap)
-                axes[i, 1].set_title(f'ground_truth|MRI|Pred,slice: {slice}, Dice: {all_dice["TP"][slice][0]}')
+                axes[i, 2].imshow(all_f1["TP"][slice][2], cmap=custom_cmap)
+                axes[i, 1].set_title(f'ground_truth|MRI|Pred,slice: {slice}, f1: {all_f1["TP"][slice][0]}')
                 axes[i, 2].axis('off')
-                z_slice_dice.append(all_dice[type][slice][0])
+                z_slice_f1.append(all_f1[type][slice][0])
             else:
                 axes[i, 1].set_title(f'Image|MRI,slice: {slice}, type {type}')
 
             # Adjust the layout and display the figure
         plt.subplots_adjust(wspace=0, hspace=0.2)
         plt.savefig(f"{out}{type}.pdf", dpi=400, bbox_inches='tight')
-    mean_dice = f"Mean common slice Dice : {np.mean(z_slice_dice)}"
+    mean_dice = f"Mean common F1 : {np.mean(z_slice_f1)}"
 
     table = [
         ['ground_truth/Pred', 'P', 'N'],
