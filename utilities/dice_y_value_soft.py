b877e7a9730696075f838a309429e494924cc05d
valosekj <jan.valosek@upol.cz>
2023-09-08 20:17:47 -0400

    Formatting fixes

--- utilities/dice_y_value_soft.py
+++ utilities/dice_y_value_soft.py
@@ -12,8 +12,8 @@ import argparse
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
 
+from matplotlib.colors import ListedColormap
 from spinalcordtoolbox.image import Image
 
 
