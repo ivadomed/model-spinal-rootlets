19dc8b8b9244fe95f3ad70c3f0e179cbf91fa200
KaterinaKrejci231054 <xkrejc78@vutbr.cz>
2024-06-19 10:12:41 -0400

    Changed variable names

--- pediatric_rootlets/pediatric_rootlets.sh
+++ pediatric_rootlets/pediatric_rootlets.sh
@@ -28,53 +28,53 @@ start=`date +%s`
 
 # Segment spinal cord if it does not exist
 segment_if_does_not_exist(){
-  FILESEG="${SUBJECT}_${FILE_TYPE}_T2w_label-SC_mask.nii.gz"
-  FILESEGMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILESEG}"
+  FILESEG="${file_t2}_label-SC_mask"
+  FILESEGMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILESEG}.nii.gz"
   echo
   echo "Looking for manual segmentation: $FILESEGMANUAL"
   if [[ -e $FILESEGMANUAL ]]; then
     echo "Found! Using manual segmentation."
-    rsync -avzh $FILESEGMANUAL ${FILESEG}
-    sct_qc -i ${file_t2} -s ${FILESEG} -p sct_deepseg_sc -qc ${PATH_QC} -qc-subject ${SUBJECT}
+    rsync -avzh $FILESEGMANUAL ${FILESEG}.nii.gz
+    sct_qc -i ${file_t2}.nii.gz -s ${FILESEG}.nii.gz -p sct_deepseg_sc -qc ${PATH_QC} -qc-subject ${SUBJECT}
   else
     echo "Not found. Proceeding with automatic segmentation."
     # Segment spinal cord
-    sct_deepseg_sc -i ${file_t2} -c t2 -qc ${PATH_QC} -qc-subject ${SUBJECT} -o ${FILESEG}
+    sct_deepseg_sc -i ${file_t2}.nii.gz -c t2 -qc ${PATH_QC} -qc-subject ${SUBJECT} -o ${FILESEG}.nii.gz
   fi
 }
 
 # Detect PMJ if it does not exist
 detect_pmj_if_does_not_exist(){
-  FILEPMJ="${SUBJECT}_${FILE_TYPE}_T2w_label-PMJ_dlabel.nii.gz"
-  FILEPMJMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILEPMJ}"
+  FILEPMJ="${file_t2}_label-PMJ_dlabel"
+  FILEPMJMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILEPMJ}.nii.gz"
   echo
   echo "Looking for manual PMJ detection: $FILEPMJMANUAL"
   if [[ -e $FILEPMJMANUAL ]]; then
     echo "Found! Using manual PMJ detection."
-    rsync -avzh $FILEPMJMANUAL ${FILEPMJ}
-    sct_qc -i ${file_t2} -s ${SUBJECT}_${FILE_TYPE}_T2w_label-SC_mask.nii.gz -d ${FILEPMJ} -p sct_detect_pmj -qc ${PATH_QC} -qc-subject ${SUBJECT}
+    rsync -avzh $FILEPMJMANUAL ${FILEPMJ}.nii.gz
+    sct_qc -i ${file_t2}.nii.gz -s ${FILESEG}.nii.gz -d ${FILEPMJ}.nii.gz -p sct_detect_pmj -qc ${PATH_QC} -qc-subject ${SUBJECT}
   else
     echo "Not found. Proceeding with automatic PMJ detection."
     # Detect PMJ
-    sct_detect_pmj -i ${file_t2} -s ${SUBJECT}_${FILE_TYPE}_T2w_label-SC_mask.nii.gz -c t2 -o ${FILEPMJ} -qc ${PATH_QC} -qc-subject ${SUBJECT}
+    sct_detect_pmj -i ${file_t2}.nii.gz -s ${FILESEG}.nii.gz -c t2 -o ${FILEPMJ}.nii.gz -qc ${PATH_QC} -qc-subject ${SUBJECT}
   fi
 }
 
 # Label vertebral levels if it does not exist
 label_if_does_not_exist(){
   # Update global variable with segmentation file name
-  FILELABEL="${SUBJECT}_${FILE_TYPE}_T2w_labels-disc.nii.gz"
-  FILELABELMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILELABEL}"
+  FILELABEL="${file_t2}_labels-disc"
+  FILELABELMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILELABEL}.nii.gz"
   echo "Looking for manual label: $FILELABELMANUAL"
   if [[ -e $FILELABELMANUAL ]]; then
     echo "Found! Using manual labels."
-    rsync -avzh $FILELABELMANUAL ${FILELABEL}
+    rsync -avzh $FILELABELMANUAL ${FILELABEL}.nii.gz
     # Generate labeled segmentation from manual disc labels
-    sct_label_vertebrae -i ${file_t2} -s ${SUBJECT}_${FILE_TYPE}_T2w_label-SC_mask.nii.gz -discfile ${FILELABEL} -c t2 -qc ${PATH_QC} -qc-subject ${SUBJECT}
+    sct_label_vertebrae -i ${file_t2}.nii.gz -s ${FILESEG}.nii.gz -discfile ${FILELABEL}.nii.gz -c t2 -qc ${PATH_QC} -qc-subject ${SUBJECT}
   else
     echo "Not found. Proceeding with automatic labeling."
     # Generate labeled segmentation
-    sct_label_vertebrae -i ${file_t2} -s ${SUBJECT}_${FILE_TYPE}_T2w_label-SC_mask.nii.gz -c t2 -qc ${PATH_QC} -qc-subject ${SUBJECT}
+    sct_label_vertebrae -i ${file_t2}.nii.gz -s ${FILESEG}.nii.gz -c t2 -qc ${PATH_QC} -qc-subject ${SUBJECT}
   fi
 }
 
@@ -94,16 +94,14 @@ rsync -Ravzh ${PATH_DATA}/./${SUBJECT}/anat/${SUBJECT//[\/]/_}_*T2w.* .
 cd ${SUBJECT}/anat
 
 # Define the names of the T2w files
-file_t2_composed=${SUBJECT}_rec-composed_T2w.nii.gz
-file_t2_top=${SUBJECT}_acq-top_run-1_T2w.nii.gz
+file_t2_composed=${SUBJECT}_rec-composed_T2w
+file_t2_top=${SUBJECT}_acq-top_run-1_T2w
 
 # Check if file_t2_composed exists, if not, use file_t2_top as file_t2
 if [ -f ${file_t2_composed} ]; then
     file_t2=${file_t2_composed}
-    FILE_TYPE="rec-composed"
 else
     file_t2=${file_t2_top}
-    FILE_TYPE="top"
     echo "Composed T2w file not found. Proceeding only with Top T2w file."
 fi
 
@@ -114,28 +112,28 @@ fi
 # https://github.com/ivadomed/model-spinal-rootlets/blob/main/packaging_ventral_rootlets/run_inference_single_subject.py
 # NOTE: we use SCT python because it has nnUNet installed
 # NOTE: the command below expects that you downloaded the model (https://github.com/ivadomed/model-spinal-rootlets/releases/tag/r20240523) and saved it to:  ~/models/model-spinal-rootlets_ventral_D106_r20240523
-$SCT_DIR/python/envs/venv_sct/bin/python ~/code/model-spinal-rootlets/packaging_ventral_rootlets/run_inference_single_subject.py -i ${file_t2} -o ${SUBJECT}_T2w_${FILE_TYPE}_label-rootlets_dseg.nii.gz -path-model ~/models/model-spinal-rootlets_ventral_D106_r20240523/model-spinal-rootlets_ventral_D106_r20240523 -fold all
+$SCT_DIR/python/envs/venv_sct/bin/python ~/code/model-spinal-rootlets/packaging_ventral_rootlets/run_inference_single_subject.py -i ${file_t2}.nii.gz -o ${file_t2}_label-rootlets_dseg.nii.gz -path-model ~/models/model-spinal-rootlets_ventral_D106_r20240523/model-spinal-rootlets_ventral_D106_r20240523 -fold all
 
  # Segment spinal cord (only if it does not exist)
-segment_if_does_not_exist ${file_t2}
+segment_if_does_not_exist ${file_t2}.nii.gz
 
 # Run sct_qc for quality control of rootlet segmentation. We are running the QC here because we need also the SC seg to
 # crop the image
-sct_qc -i ${file_t2} -s ${SUBJECT}_${FILE_TYPE}_T2w_label-SC_mask.nii.gz -d ${SUBJECT}_T2w_${FILE_TYPE}_label-rootlets_dseg.nii.gz -p sct_deepseg_lesion -qc ${PATH_QC} -qc-subject ${SUBJECT} -plane axial
+sct_qc -i ${file_t2}.nii.gz -s ${file_t2}_label-SC_mask.nii.gz -d ${file_t2}_label-rootlets_dseg.nii.gz -p sct_deepseg_lesion -qc ${PATH_QC} -qc-subject ${SUBJECT} -plane axial
 
 # Run sct_label_vertebrae for vertebral levels estimation
-label_if_does_not_exist ${file_t2}
+label_if_does_not_exist ${file_t2}.nii.gz
 
 # Detect PMJ (only if it does not exist)
-detect_pmj_if_does_not_exist ${file_t2}
+detect_pmj_if_does_not_exist ${file_t2}.nii.gz
 
 # Get rootlets spinal levels
 # Note: we use SCT python because the `02a_rootlets_to_spinal_levels.py` script imports some SCT classes
-$SCT_DIR/python/envs/venv_sct/bin/python ~/code/model-spinal-rootlets/inter-rater_variability/02a_rootlets_to_spinal_levels.py -i ${SUBJECT}_T2w_${FILE_TYPE}_label-rootlets_dseg.nii.gz -s ${SUBJECT}_${FILE_TYPE}_T2w_label-SC_mask.nii.gz -pmj ${SUBJECT}_${FILE_TYPE}_T2w_label-PMJ_dlabel.nii.gz -type rootlets
+$SCT_DIR/python/envs/venv_sct/bin/python ~/code/model-spinal-rootlets/inter-rater_variability/02a_rootlets_to_spinal_levels.py -i ${file_t2}_label-rootlets_dseg.nii.gz -s ${file_t2}_label-SC_mask.nii.gz -pmj ${file_t2}_label-PMJ_dlabel.nii.gz -type rootlets
 
 # Get vertebral spinal levels - with cropping parts, where are more than just 2 levels (background and level)
 # Note: we use SCT python because the `02a_rootlets_to_spinal_levels.py` script imports some SCT classes
-$SCT_DIR/python/envs/venv_sct/bin/python ~/code/model-spinal-rootlets/inter-rater_variability/02a_rootlets_to_spinal_levels.py -i ${SUBJECT}_${FILE_TYPE}_T2w_label-SC_mask_labeled.nii.gz -s ${SUBJECT}_${FILE_TYPE}_T2w_label-SC_mask.nii.gz -pmj ${SUBJECT}_${FILE_TYPE}_T2w_label-PMJ_dlabel.nii.gz -type vertebral
+$SCT_DIR/python/envs/venv_sct/bin/python ~/code/model-spinal-rootlets/inter-rater_variability/02a_rootlets_to_spinal_levels.py -i ${file_t2}_label-SC_mask_labeled.nii.gz -s ${file_t2}_label-SC_mask.nii.gz -pmj ${file_t2}_label-PMJ_dlabel.nii.gz -type vertebral
 
 
 # Display useful info for the log
