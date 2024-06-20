0336cfb999c2e3a2d461c7bdc4e6a45371beb384
KaterinaKrejci231054 <xkrejc78@vutbr.cz>
2024-06-20 12:08:58 -0400

    Added cropping of composed images to the size of top images

--- pediatric_rootlets/pediatric_rootlets.sh
+++ pediatric_rootlets/pediatric_rootlets.sh
@@ -130,14 +130,30 @@ label_if_does_not_exist ${file_t2}.nii.gz
 # Detect PMJ (only if it does not exist)
 detect_pmj_if_does_not_exist ${file_t2}.nii.gz
 
-# Get rootlets spinal levels
-# Note: we use SCT python because the `02a_rootlets_to_spinal_levels.py` script imports some SCT classes
-$SCT_DIR/python/envs/venv_sct/bin/python ~/code/model-spinal-rootlets/inter-rater_variability/02a_rootlets_to_spinal_levels.py -i ${file_t2}_label-rootlets_dseg.nii.gz -s ${file_t2}_label-SC_mask.nii.gz -pmj ${file_t2}_label-PMJ_dlabel.nii.gz -type rootlets
+# Crop composed images and labels to the size of the top image (if the composed image exists)
+if [ -f ${file_t2_composed}.nii.gz ]; then
+  $SCT_DIR/python/envs/venv_sct/bin/python ~/code/model-spinal-rootlets/pediatric_rootlets/crop_composed_images.py -i_composed ${file_t2_composed}.nii.gz -i_top ${file_t2_top}.nii.gz -i_composed_label ${file_t2_composed}.nii.gz -s ${FILESEG}.nii.gz -d ${FILELABEL}.nii.gz -pmj ${FILEPMJ}.nii.gz -rootlets_seg ${file_t2}_label-rootlets_dseg.nii.gz
 
-# Get vertebral spinal levels - with cropping parts, where are more than just 2 levels (background and level)
-# Note: we use SCT python because the `02a_rootlets_to_spinal_levels.py` script imports some SCT classes
-# $SCT_DIR/python/envs/venv_sct/bin/python ~/code/model-spinal-rootlets/inter-rater_variability/02a_rootlets_to_spinal_levels.py -i ${file_t2}_label-SC_mask_labeled.nii.gz -s ${file_t2}_label-SC_mask.nii.gz -pmj ${file_t2}_label-PMJ_dlabel.nii.gz -type vertebral
-$SCT_DIR/python/envs/venv_sct/bin/python ~/code/model-spinal-rootlets/pediatric_rootlets/vertebrae_to_spinal_levels.py -centerline ${file_t2}_label-SC_mask_centerline_extrapolated.csv -disclabel ${file_t2}_labels-disc.nii.gz
+  # Get rootlets spinal levels from cropped images
+  # Note: we use SCT python because the `02a_rootlets_to_spinal_levels.py` script imports some SCT classes
+  $SCT_DIR/python/envs/venv_sct/bin/python ~/code/model-spinal-rootlets/inter-rater_variability/02a_rootlets_to_spinal_levels.py -i ${file_t2}_label-rootlets_dseg_crop.nii.gz -s ${file_t2}_label-SC_mask_crop.nii.gz -pmj ${file_t2}_label-PMJ_dlabel_crop.nii.gz -type rootlets
+
+  # Get vertebral spinal levels according to intervertebral discs
+  # Note: we use SCT python because the `02a_rootlets_to_spinal_levels.py` script imports some SCT classes
+  # $SCT_DIR/python/envs/venv_sct/bin/python ~/code/model-spinal-rootlets/inter-rater_variability/02a_rootlets_to_spinal_levels.py -i ${file_t2}_label-SC_mask_labeled.nii.gz -s ${file_t2}_label-SC_mask.nii.gz -pmj ${file_t2}_label-PMJ_dlabel.nii.gz -type vertebral
+  $SCT_DIR/python/envs/venv_sct/bin/python ~/code/model-spinal-rootlets/pediatric_rootlets/vertebrae_to_spinal_levels.py -centerline ${file_t2}_label-SC_mask_centerline_extrapolated.csv -disclabel ${file_t2}_labels-disc_crop.nii.gz
+
+else
+  # Get rootlets spinal levels
+  # Note: we use SCT python because the `02a_rootlets_to_spinal_levels.py` script imports some SCT classes
+  $SCT_DIR/python/envs/venv_sct/bin/python ~/code/model-spinal-rootlets/inter-rater_variability/02a_rootlets_to_spinal_levels.py -i ${file_t2}_label-rootlets_dseg.nii.gz -s ${file_t2}_label-SC_mask.nii.gz -pmj ${file_t2}_label-PMJ_dlabel.nii.gz -type rootlets
+
+  # Get vertebral spinal levels - with cropping parts, where are more than just 2 levels (background and level)
+  # Note: we use SCT python because the `02a_rootlets_to_spinal_levels.py` script imports some SCT classes
+  # $SCT_DIR/python/envs/venv_sct/bin/python ~/code/model-spinal-rootlets/inter-rater_variability/02a_rootlets_to_spinal_levels.py -i ${file_t2}_label-SC_mask_labeled.nii.gz -s ${file_t2}_label-SC_mask.nii.gz -pmj ${file_t2}_label-PMJ_dlabel.nii.gz -type vertebral
+  $SCT_DIR/python/envs/venv_sct/bin/python ~/code/model-spinal-rootlets/pediatric_rootlets/vertebrae_to_spinal_levels.py -centerline ${file_t2}_label-SC_mask_centerline_extrapolated.csv -disclabel ${file_t2}_labels-disc.nii.gz
+
+fi
 
 # Display useful info for the log
 end=`date +%s`
