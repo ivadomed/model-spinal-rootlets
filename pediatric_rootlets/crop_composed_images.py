f6e2e511d4207598bfce6d3ff80c25487a21af60
KaterinaKrejci231054 <xkrejc78@vutbr.cz>
2024-07-02 11:30:17 -0400

    Changed cropping to zeroing

--- pediatric_rootlets/crop_composed_images.py
+++ pediatric_rootlets/crop_composed_images.py
@@ -25,24 +25,14 @@ def get_parser():
         prog=os.path.basename(__file__)
     )
     parser.add_argument(
-        '-i-composed',
+        '-rootlets-seg',
         required=True,
         help='Path to the T2w composed image.'
-    )
-    parser.add_argument(
-        '-s',
-        required=True,
-        help='Path to the segmentation mask.'
     )
     parser.add_argument(
         '-d',
         required=True,
         help='Path to the disc labels.'
-    )
-    parser.add_argument(
-        '-pmj',
-        required=True,
-        help='Path to the PMJ file.'
     )
     parser.add_argument(
         '-x',
@@ -58,14 +48,13 @@ def main():
     parser = get_parser()
     args = parser.parse_args()
 
-    composed_image = args.i_composed
-    seg_mask = args.s
+    rootlets_seg = args.rootlets_seg
     disc_labels = args.d
-    pmj = args.pmj
-    x= int(args.x)
+    x = int(args.x)
 
     # Load the images and change orientation to RPI
     disc_labels_RPI= Image(disc_labels).change_orientation('RPI')
+    rootlets_seg_RPI = Image(rootlets_seg).change_orientation('RPI')
 
     # find the coordinates, where is the disc label x
     disc_label_x = np.where(disc_labels_RPI.data == x)
@@ -73,23 +62,14 @@ def main():
     # Set the level, where to crop the images
     cropping_level = disc_label_x[2][0]
 
-    # Get name of the cropped images and masks
-    crop_composed_image = composed_image.replace('.nii.gz', '_crop.nii.gz')
-    crop_seg_mask= seg_mask.replace('.nii.gz', '_crop.nii.gz')
-    crop_disc_labels = disc_labels.replace('.nii.gz', '_crop.nii.gz')
-    crop_pmj = pmj.replace('.nii.gz', '_crop.nii.gz')
+    disc_label_x = np.where(disc_labels_RPI.data == x)
 
-    # Crop the composed image
-    os.system('sct_crop_image -i ' + composed_image + ' -o ' + crop_composed_image + ' -ymin ' + str(cropping_level))
+    # Put zeros to the rootlets segmentation under the disc level x
+    rootlets_seg_RPI.data[:, :, cropping_level:] = 0
 
-    # Crop the segmentation mask
-    os.system('sct_crop_image -i ' + seg_mask + ' -o ' + crop_seg_mask + ' -ymin ' + str(cropping_level))
-
-    # Crop the disc labels
-    os.system('sct_crop_image -i ' + disc_labels + ' -o ' + crop_disc_labels + ' -ymin ' + str(cropping_level))
-
-    # Crop the PMJ
-    os.system('sct_crop_image -i ' + pmj + ' -o ' + crop_pmj + ' -ymin ' + str(cropping_level))
+    rootlets_seg_modif = rootlets_seg.replace('.nii.gz', '_modif.nii.gz')
+    # Save the modified segmentation
+    rootlets_seg_RPI.save(rootlets_seg_modif, dtype=np.uint8)
 
 if __name__ == '__main__':
     main()
