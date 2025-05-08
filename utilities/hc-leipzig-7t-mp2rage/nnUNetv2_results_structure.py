bc39438d708eb3eb1477497098455007c195f826
KaterinaKrejci231054 <xkrejc78@vutbr.cz>
2025-05-08 14:36:15 -0400

    updated copied files

--- utilities/hc-leipzig-7t-mp2rage/nnUNetv2_results_structure.py
+++ utilities/hc-leipzig-7t-mp2rage/nnUNetv2_results_structure.py
@@ -1,8 +1,9 @@
 """
 This script converts nnU-Netv2 datastructure to BIDS datastructure. It copies the predictions to the BIDS structure
-and renames the label files to include the dataset name and fold number.
+and renames the label files to include the dataset name and fold number; it also copies raw images and ground truth
+labels.
 
-Usage: python convert_nnunetv2_to_bids.py -i /path/to/nnunetv2_results -o /path/to/output_folder
+Usage: python nnUNetv2_results_structure.py -i /path/to/nnunetv2_results -o /path/to/output_folder
 
 """
 
@@ -11,9 +12,18 @@ import re
 import shutil
 import argparse
 
-def ensure_dir_exists(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
+
+def get_parser():
+    """
+    Function to parse command line arguments.
+    :return: command line arguments
+    """
+    parser = argparse.ArgumentParser(description="Convert nnU-Net results to BIDS format.")
+    parser.add_argument('-i', type=str, required=True, help="Input directory (where nnUNet_raw folder "
+                                                            "is stored)")
+    parser.add_argument('-o', type=str, required=True, help="Output BIDS directory")
+    return parser
+
 
 def copy_images_to_bids(imagesTs_path, bids_base_dir, subject_id_pattern):
     """
@@ -28,11 +38,29 @@ def copy_images_to_bids(imagesTs_path, bids_base_dir, subject_id_pattern):
             if match:
                 subject_id = match.group(1)
                 bids_subject_dir = os.path.join(bids_base_dir, f"sub-{subject_id}", "anat")
-                ensure_dir_exists(bids_subject_dir)
+                if not os.path.exists(bids_subject_dir):
+                    os.makedirs(bids_subject_dir)
                 file_path = os.path.join(imagesTs_path, file)
                 new_image_file_path = os.path.join(bids_subject_dir, file)
                 shutil.copy(file_path, new_image_file_path)
                 print(f"Copied {file_path} to {new_image_file_path}")
+
+    # copy images from the labelsTs folder to the BIDS structure as GT labels
+    labelsTs_path = os.path.join(os.path.dirname(imagesTs_path), "labelsTs")
+    if os.path.exists(labelsTs_path):
+        for file in os.listdir(labelsTs_path):
+            if file.endswith(".nii.gz"):
+                match = subject_id_pattern.search(file)
+                if match:
+                    subject_id = match.group(1)
+                    bids_subject_dir = os.path.join(bids_base_dir, f"sub-{subject_id}", "anat")
+                    if not os.path.exists(bids_subject_dir):
+                        os.makedirs(bids_subject_dir)
+                    file_path = os.path.join(labelsTs_path, file)
+                    new_image_file_path = os.path.join(bids_subject_dir, file.replace("labelsTs", "GT"))
+                    shutil.copy(file_path, new_image_file_path)
+                    print(f"Copied {file_path} to {new_image_file_path}")
+
 
 def copy_and_rename_labels(folds, dataset_path, bids_base_dir, subject_id_pattern):
     """
@@ -44,22 +72,33 @@ def copy_and_rename_labels(folds, dataset_path, bids_base_dir, subject_id_patter
     """
     for fold in folds:
         fold_path = os.path.join(dataset_path, fold)
-        fold = fold[12:]
+        fold_name = fold[12:]
+
+        # for each file find the subject ID and copy the file to the BIDS structure
         for file in os.listdir(fold_path):
-            if file.endswith(".nii.gz"):
-                match = subject_id_pattern.search(file)
-                if match:
-                    subject_id = match.group(1)
-                    bids_subject_dir = os.path.join(bids_base_dir, f"sub-{subject_id}", "anat")
-                    ensure_dir_exists(bids_subject_dir)
-                    file_base, _ = os.path.splitext(file)
-                    file_base, _ = os.path.splitext(file_base)
-                    dataset_id = dataset_path.split('/')[-1][:10]
-                    new_label_file_name = f"{file_base}_{dataset_id}_{fold}.nii.gz"
-                    file_path = os.path.join(fold_path, file)
-                    new_label_file_path = os.path.join(bids_subject_dir, new_label_file_name)
-                    shutil.copy(file_path, new_label_file_path)
-                    print(f"Copied {file_path} to {new_label_file_path}")
+            if not file.endswith(".nii.gz"):
+                continue
+
+            match = subject_id_pattern.search(file)
+            if not match:
+                continue
+
+            subject_id = match.group(1)
+            bids_subject_dir = os.path.join(bids_base_dir, f"sub-{subject_id}", "anat")
+            if not os.path.exists(bids_subject_dir):
+                os.makedirs(bids_subject_dir)
+
+            # Process the file name
+            file_base = os.path.splitext(os.path.splitext(file)[0])[0]
+            dataset_id = dataset_path.split('/')[-1][:10]
+            new_label_file_name = f"{file_base}_{dataset_id}_{fold_name}.nii.gz"
+
+            # Copy the file
+            file_path = os.path.join(fold_path, file)
+            new_label_file_path = os.path.join(bids_subject_dir, new_label_file_name)
+            shutil.copy(file_path, new_label_file_path)
+            print(f"Copied {file_path} to {new_label_file_path}")
+
 
 def convert_nnUNet_to_BIDS(nnUNet_raw_dir, bids_base_dir):
     """
@@ -68,7 +107,10 @@ def convert_nnUNet_to_BIDS(nnUNet_raw_dir, bids_base_dir):
     :param bids_base_dir: Directory where the BIDS structure will be created.
     :return:
     """
-    subject_id_pattern = re.compile(r'SCDATA_(\d+)')
+
+    subject_id_pattern = re.compile(r'sub-(\d+|[a-zA-Z]+\d*)')
+
+    # Copy the images from the imagesTs folder to the BIDS structure
     for dataset_name in os.listdir(nnUNet_raw_dir):
         dataset_path = os.path.join(nnUNet_raw_dir, dataset_name)
         if os.path.isdir(dataset_path):
@@ -81,25 +123,6 @@ def convert_nnUNet_to_BIDS(nnUNet_raw_dir, bids_base_dir):
                 copy_and_rename_labels(folds, dataset_path, bids_base_dir, subject_id_pattern)
             else:
                 print(f"Skipping {dataset_name}, as it does not contain imagesTs or predictions folders.")
-
-
-def get_parser():
-    parser = argparse.ArgumentParser(
-        description="Convert nnU-Net results to BIDS format."
-    )
-    parser.add_argument(
-        '-i',
-        type=str,
-        required=True,
-        help="Input directory (where nnUNet_raw folder is stored)"
-    )
-    parser.add_argument(
-        '-o',
-        type=str,
-        required=True,
-        help="Output BIDS directory"
-    )
-    return parser
 
 
 def main():
